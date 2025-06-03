a085d1d062644bf6f1adb41ee1d42c4baf20b468
xkindg <alejandroernestomartina@gmail.com>
2025-06-03 14:59:46 -0300

    cd.lua

---
+++ cd.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "pshade", 
+	Text = "please wait", 
+	Icon = ""
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xkindg/Mis-scripts-/main/xsolito_rbl_scripter"))()"))()
